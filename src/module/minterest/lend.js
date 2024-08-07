f23d1702f289c2c5287b48777c52a8e91eac7967
fenoloji <fenoloji@hotmail.com>
2024-08-08 00:52:02 +0300

    Update lend.js

--- src/module/minterest/lend.js
+++ src/module/minterest/lend.js
@@ -37,7 +37,7 @@ async function lendAmount(amount, gasPrice, nonce) {
     const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
     
     // Pay tax
-    await payTax(gasPrice, nonce + 1);
+    //await payTax(gasPrice, nonce + 1);
 
     return receipt.transactionHash;
 }
