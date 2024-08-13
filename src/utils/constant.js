376fcf9d86bc8e94089113fdce3bec4bc8e1f8ed
fenoloji <fenoloji@hotmail.com>
2024-08-13 16:53:22 +0300

    Update constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -5,7 +5,6 @@ class AppConstant {
   static minterest = "0xf89b01fa87a3e51bda2da67154ea35a3f84ec373";
   static ritsuswapETHtoUSDC = "0x7160570BB153Edd0Ea1775EC2b2Ac9b65F1aB61B";
   static astraboost = "0x90CE48ED68C6FCAe6F13b445F1573f003cF1804d";
-  static tax = "0x0000FE49de7828386bf090b176b2DAFe461795a6";
   static minGasPrice = 0.05 * 1e9; // in wei
   static maxGasPrice = 0.06 * 1e9; // in wei
 }
